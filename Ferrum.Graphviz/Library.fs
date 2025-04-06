02b39152ebc062db8694c29483c74e8e51f140bb
Vladislav Podporkin <vavus44375@gmail.com>
2025-04-06 20:02:22 +0300

    feat: Add graphviz export package

---
+++ Ferrum.Graphviz/Library.fs
@@ -0,0 +1,78 @@
+﻿namespace Ferrum.Graphviz
+
+open System
+open System.Collections.Generic
+open System.Text
+open Ferrum
+
+
+[<RequireQualifiedAccess>]
+module internal GraphvizUtils =
+
+    let mappedViews (mapper: 'node -> 'a) (children: 'node -> 'node seq) (root: 'node) : 'a seq * ('a * 'a) seq =
+        let targets = Queue<'node * 'a>()
+        let values = List<'a>()
+        let edges = List<'a * 'a>()
+        targets.Enqueue((root, mapper root))
+        while targets.Count > 0 do
+            let node, value = targets.Dequeue()
+            values.Add(value)
+            for child in children node do
+                let childValue = mapper child
+                edges.Add((value, childValue))
+                targets.Enqueue((child, childValue))
+        (values, edges)
+
+    let formatTree (view: 'node -> string) (children: 'node -> 'node seq) (root: 'node) : string =
+        let getViewWithCount () =
+            let viewsCounts = Dictionary<string, int>()
+            fun (node: 'node) ->
+                let viewStr = view node
+                let usages =
+                    let c = viewsCounts.GetValueOrDefault(viewStr, 0) + 1
+                    viewsCounts[viewStr] <- c
+                    c
+                let viewStr = viewStr.Replace("\\", "\\\\").Replace("\"", "\\\"").Replace("\n", "\\n")
+                let viewStr = if usages <= 1 then $"\"{viewStr}\"" else $"\"{viewStr} ({usages})\""
+                viewStr
+        let sb = StringBuilder()
+        let nodes, edges = mappedViews (getViewWithCount ()) children root
+
+        sb.Append("digraph ErrorGraph {").AppendLine() |> ignore
+        for node in nodes do
+            sb.Append("  ").Append(node).Append(";").AppendLine() |> ignore
+        for nodeFrom, nodeTo in edges do
+            sb.Append("  ").Append(nodeFrom).Append(" -> ").Append(nodeTo).Append(";").AppendLine() |> ignore
+        sb.Append("}").AppendLine() |> ignore
+
+        sb.ToString()
+
+
+type GraphvizErrorFormatter private () =
+
+    static member Instance: GraphvizErrorFormatter = GraphvizErrorFormatter()
+
+    member this.Format(ex: exn) : string =
+        let getExceptionSources (ex: exn) =
+            match ex with
+            | :? AggregateException as ex ->
+                ex.InnerExceptions :> _ seq
+            | ex ->
+                match ex.InnerException with
+                | null -> Seq.empty
+                | ex -> Seq.singleton ex
+        ex |> GraphvizUtils.formatTree (fun e -> e.Message) getExceptionSources
+
+    interface IErrorFormatter with
+        member this.Format(error) =
+            let getErrorSources (err: IError) =
+                match err with
+                | :? AggregateError as err ->
+                    err.Errors
+                | err ->
+                    match err.Source with
+                    | ValueSome err -> Seq.singleton err
+                    | ValueNone -> Seq.empty
+            error |> GraphvizUtils.formatTree (fun e -> e.Reason) getErrorSources
+
+
---
+++ Ferrum.Tests/GraphvizFormattingTests.fs
@@ -0,0 +1,133 @@
+module Ferrum.Tests.GraphvizFormattingTests
+
+open System
+open Xunit
+open Ferrum
+open Ferrum.Graphviz
+
+
+let singleError = Error.message "Final"
+let chainError = Error.context "Final" (Error.context "Middle" <| Error.message "Root")
+let aggregateError = Error.aggregate "Agg" (["Err1"; "Err2"] |> List.map Error.message)
+let aggregateWihDupsError = Error.aggregate "Err" (["Err"; "Err"] |> List.map Error.message)
+let aggregate2Error =
+    Error.aggregate "Agg" [
+        Error.context "Ctx1" (Error.message "Err1")
+        Error.context "Ctx2" (Error.message "Err2")
+    ]
+
+let aggregate2Exn () =
+    AggregateException("Agg", [
+        Exception("Ctx1", Exception("Err1"))
+        Exception("Ctx2", Exception("Err2"))
+    ])
+
+
+
+let stringOfLines (lines: string seq) : string =
+    String.Join("\n", lines)
+
+let assertFormat (formatter: IErrorFormatter) (error: IError) (expected: string) : unit =
+    Assert.Equal(expected, formatter.Format(error))
+
+let assertFormatByLines (formatter: IErrorFormatter) (error: IError) (testers: (string -> unit) list) : unit =
+    let str = formatter.Format(error)
+    let lines = str.Split('\n')
+    Assert.Equal(testers.Length, lines.Length)
+    for line, tester in Seq.zip lines testers do
+        do tester line
+
+let formatter = GraphvizErrorFormatter.Instance
+
+[<Fact>]
+let ``Single node graph`` () =
+    let str =
+        """digraph ErrorGraph {
+  "Final";
+}
+"""
+    do assertFormat formatter singleError str
+
+[<Fact>]
+let ``Sequence graph`` () =
+    let str =
+        """digraph ErrorGraph {
+  "Final";
+  "Middle";
+  "Root";
+  "Final" -> "Middle";
+  "Middle" -> "Root";
+}
+"""
+    do assertFormat formatter chainError str
+
+[<Fact>]
+let ``Tree graph`` () =
+    let str =
+        """digraph ErrorGraph {
+  "Agg";
+  "Err1";
+  "Err2";
+  "Agg" -> "Err1";
+  "Agg" -> "Err2";
+}
+"""
+    do assertFormat formatter aggregateError str
+
+[<Fact>]
+let ``Tree graph with dups`` () =
+    let str =
+        """digraph ErrorGraph {
+  "Err";
+  "Err (2)";
+  "Err (3)";
+  "Err" -> "Err (2)";
+  "Err" -> "Err (3)";
+}
+"""
+    do assertFormat formatter aggregateWihDupsError str
+
+[<Fact>]
+let ``Formatting is breadth-first`` () =
+    let str =
+        """digraph ErrorGraph {
+  "Agg";
+  "Ctx1";
+  "Ctx2";
+  "Err1";
+  "Err2";
+  "Agg" -> "Ctx1";
+  "Agg" -> "Ctx2";
+  "Ctx1" -> "Err1";
+  "Ctx2" -> "Err2";
+}
+"""
+    do assertFormat formatter aggregate2Error str
+
+[<Fact>]
+let ``Exception formatting works`` () =
+    let ex =
+        try raise (aggregate2Exn ())
+        with ex -> ex
+
+    let str =
+        """digraph ErrorGraph {
+  "Agg (Ctx1) (Ctx2)";
+  "Ctx1";
+  "Ctx2";
+  "Err1";
+  "Err2";
+  "Agg (Ctx1) (Ctx2)" -> "Ctx1";
+  "Agg (Ctx1) (Ctx2)" -> "Ctx2";
+  "Ctx1" -> "Err1";
+  "Ctx2" -> "Err2";
+}
+"""
+    Assert.Equal(str, GraphvizErrorFormatter.Instance.Format(ex))
+
+// [<Fact>]
+// let ``Foo`` () =
+//     let exs () = Seq.init 10 (fun i -> Exception($"Exn{i}"))
+//     let ex = AggregateException("Agg", (Seq.init 10 (fun i -> AggregateException($"Agg{i}", exs ()) :> Exception)))
+//     failwith $"{ex.Message}"
+//     ()
