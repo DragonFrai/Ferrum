1e7e5254087207745b5cd87c3848dd774db734d7
Vladislav Podporkin <vavus44375@gmail.com>
2025-07-16 01:33:22 +0300

    refactor(Ferrum.Graphviz): Reimplement on C#

---
+++ Ferrum.Graphviz/GraphvizErrorFormatter.cs
@@ -0,0 +1,31 @@
+﻿using Ferrum.Formatting;
+
+namespace Ferrum.Graphviz;
+
+public class GraphvizErrorFormatter : IErrorFormatter
+{
+    private GraphvizErrorFormatter() {}
+
+    public static readonly GraphvizErrorFormatter Instance = new();
+
+    public string Format(IError error)
+    {
+        return Helpers.FormatTree(err => err.Message, err => err.GetInnerErrors(), error);
+    }
+
+    public string Format(Exception exception)
+    {
+        static IEnumerable<Exception> GetInners(Exception ex)
+        {
+            switch (ex)
+            {
+                case AggregateException aggregateException:
+                    return aggregateException.InnerExceptions;
+                default:
+                    var innerException = ex.InnerException;
+                    return innerException is not null ? [innerException] : [];
+            }
+        }
+        return Helpers.FormatTree(ex => ex.Message, GetInners, exception);
+    }
+}
